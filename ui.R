40ba2a7c7964e3e00b3a2759eacf3a2951d132dd
Nalinitak <88829834+Nalinitak@users.noreply.github.com>
2023-02-20 11:35:37 +0530

    Dispay data set and its summery

---
+++ ui.R
@@ -0,0 +1,11 @@
+install.packages("shiny")
+library(shiny)
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+
+
+
